3af0de57ed2fd78510f8e94869a69d4619a5b9d7
Karan Anand <anandkarancompsci@gmail.com>
2023-04-12 14:53:49 -0700

    Improved code coverage

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for the Event class
  */
 public class EventTest {
-    private Event e;
+    private Event e, c1, c2, c3, c4;
     private Date d;
 
     //NOTE: these tests might fail if time at which line (2) below is executed
@@ -22,6 +22,10 @@ public class EventTest {
     @BeforeEach
     public void runBefore() {
         e = new Event("Sensor open at door");   // (1)
+        c1 = new Event("1111");
+        c2 = new Event("1111");
+        c3 = new Event("1234");
+        c4 = new Event("twelve");
         d = Calendar.getInstance().getTime();   // (2)
     }
 
@@ -29,6 +33,19 @@ public class EventTest {
     public void testEvent() {
         assertEquals("Sensor open at door", e.getDescription());
         assertEquals(d, e.getDate());
+    }
+
+    @Test
+    public void testHashCode() {
+        assertTrue(c1.hashCode() == c2.hashCode());
+        assertFalse(c1.hashCode() == c3.hashCode());
+    }
+
+    @Test
+    public void testEqualsObject() {
+        assertTrue(c1.equals(c1));
+        assertTrue(c1.equals(c2));
+        assertFalse(c1.equals(c3));
     }
 
     @Test
